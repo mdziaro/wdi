2b3a0a8499fcd1f4f36ce467cd305e8fc93058a0
mdziaro <mdziaro@student.agh.edu.pl>
2022-10-15 16:05:30 +0200

    cw3

---
+++ cw4.py
@@ -0,0 +1,18 @@
+print("Michał")
+print("Dziarkowski")
+print(19)
+print("Pizza")
+print("Wombat")
+print(5/7)
+print("Michał", "Dziarkowski", 19, "Pizza", "Wombat", 5/7)
+imie = "Michał"
+nazwisko ="Dziarkowski"
+wiek =19
+jedzenie ="Pizza"
+zwierze = "Wombat"
+wynik = 5/7
+print(imie, nazwisko, wiek, jedzenie, zwierze, wynik)
+print(round(wynik,1))
+print(round(wynik,3))
+print(round(wynik,5))
+print(round(wynik,10))
