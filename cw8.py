ac88280b515bdef5cf2560a41bf5f37eba9147de
mdziaro <mdziaro@student.agh.edu.pl>
2022-10-18 21:06:25 +0200

    cw8

--- cw8.py
+++ cw8.py
@@ -1,10 +1,10 @@
 liczba =int(input())
-
+import random
 def choinka(a):
     print(" "*((a-1)), "X")
     a *= 2
     for i in range(3, a, 2):
-        m = ((((a-1)**(a))) % (i-1))
+        m = random.randint(1,i)
         print(" "*(((a-i)//2)), (i-m)*"*" + "o" + "*"*(m-1))
     print(" " * ((a-1)//2), "U")
 choinka(liczba)
