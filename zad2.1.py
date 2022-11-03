5fac863c68d240ea2f4f3dd90442468c332a2a1c
mdziaro <mdziaro@student.agh.edu.pl>
2022-11-03 10:52:01 +0100

    zad2.1 dodałem TenToBase oraz Główną funkcję

--- zad2.1.py
+++ zad2.1.py
@@ -4,6 +4,10 @@ z jednego systemu liczb w drugi w zależności od wyboru użytkownika (na wejśc
 Należy rozważyć następujące systemy liczbowe: dwójkowy, ósemkowy, dziesiętny, szesnastkowy.
 '''
 
+liczba = input("Podaj liczbę, w systemie, w którym jest zapisana")
+baza_pocz = int(input("Podaj system, w którym jest zapisana liczba"))
+baza_konc = int(input("Podaj system, na który chcesz zamienić liczbę"))
+
 def BaseToTen(baza, liczba):
     wynik = 0
     iteracja = 0
@@ -19,5 +23,24 @@ def BaseToTen(baza, liczba):
         kopia_liczba = kopia_liczba[:-1]
     return wynik
 
-print(BaseToTen(13, "AC3"))
-def TenToBase
+#print(BaseToTen(16, "AC3"))
+def TenToBase(baza, liczba):
+    wynik = []
+    liczba_kopia = liczba
+    while liczba_kopia>0:
+        wynik.append(liczba_kopia%baza)
+        liczba_kopia //= baza
+    wynik = wynik[::-1]
+    return wynik
+
+def ZamianaBaz(baza_pocz, liczba, baza_konc):
+    wynik=""
+    tablica = TenToBase(baza_konc,BaseToTen(baza_pocz, liczba))
+    for i in range(len(tablica)):
+        if tablica[i] < 10:
+            wynik += str(tablica[i])
+        else:
+            wynik += chr(tablica[i]-11 + ord("A"))
+    return wynik
+print(ZamianaBaz(baza_pocz,liczba,baza_konc))
+
